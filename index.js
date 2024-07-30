eb533e1ad3aa0bfd208212d9954502df08b41351
unknown <atikurahad@gmail.com>
2024-07-30 18:23:53 +0600

    server create successfully

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+ *Title: Uptime monitoring Application
+ * Description: A RESTFul API to monitor up or down time of user defined links
+ * Author: Atikur Rahaman Ahad
+ * Date: 7/30/2024
+ */
+
+// dependecies
+
+const http = require("http");
+
+//app object - scaffolding
+const app = {};
+//configaration
+app.config = {
+  port: 3001,
+};
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`Listening to server on port${app.config.port}`);
+  });
+};
+
+// handle request response
+
+app.handleReqRes = (req, res) => {
+  //response handle
+  res.end("Hello Node Js!");
+};
+
+
+// start the server
+app.createServer();
